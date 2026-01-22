856833f9521b8c2debf175bf85c3004ed19ac42c
Ostap Haivanovych <97313368+9uyone@users.noreply.github.com>
2026-01-22 21:32:03 +0200

    feat: add FindAsync to MongoRepository

--- src/Common/Interfaces/IMongoRepository.cs
+++ src/Common/Interfaces/IMongoRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace Common.Interfaces;
+﻿using System.Linq.Expressions;
+
+namespace Common.Interfaces;
 
 public interface IMongoRepository<T> where T : class {
 	Task CreateAsync(T entity);
 	Task<List<T>> GetAllAsync();
 	Task<List<T>> GetBySourceAsync(string source, int? page, int? pageSize);
+	Task<List<T>> FindAsync(Expression<Func<T, bool>> filter);
 }
--- src/Common/Repositories/MongoRepository.cs
+++ src/Common/Repositories/MongoRepository.cs
@@ -1,13 +1,16 @@
 ﻿using Common.Interfaces;
-using MongoDB.Driver;
 using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Linq.Expressions;
 
 namespace Common.Repositories;
 
 public class MongoRepository<T>(IMongoDatabase database, string collectionName) : IMongoRepository<T> where T : class {
 	private readonly IMongoCollection<T> _collection = database.GetCollection<T>(collectionName);
 
-	public async Task CreateAsync(T entity) => await _collection.InsertOneAsync(entity);
+	public async Task CreateAsync(T entity) => 
+		await _collection.InsertOneAsync(entity);
+
 	public async Task<List<T>> GetAllAsync() => await _collection.Find(_ => true).ToListAsync();
 
 	public async Task<List<T>> GetBySourceAsync(string source, int? page = 1, int? pageSize = 20) {
@@ -17,4 +20,7 @@ public class MongoRepository<T>(IMongoDatabase database, string collectionName) 
 			.Limit(pageSize)
 			.ToListAsync();
 	}
+
+	public async Task<List<T>> FindAsync(Expression<Func<T, bool>> filter) =>
+		await _collection.Find(filter).ToListAsync();
 }
